abbb5668764e1a8ee94f4ce128033708e92b8356
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-28 23:07:37 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,5 +1,5 @@
-require_relative './lib/customer.rb'
-require_relative './lib/meal.rb'
+#require_relative './lib/customer.rb'
+#require_relative './lib/meal.rb'
 
 class Waiter
   attr_accessor :name, :yrs_experience 
